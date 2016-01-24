e5f069338875aac7ee845adf40201c3e8b262ea7
Jaakko Husso <jaakko.husso@student.tut.fi>
2016-01-24 20:27:14 +0200

    Added new permissions for vastuuhenkilo.

--- vastuuhenkiloschema.sql
+++ vastuuhenkiloschema.sql
@@ -8,6 +8,18 @@ GRANT USAGE ON SCHEMA vastuuhenkilo_rajattu TO vastuuhenkilo;
 -- Vastuuhenkilo needs all basic CRUD operations on TAPAHTUMA
 GRANT SELECT, INSERT, UPDATE, DELETE ON TAPAHTUMA TO vastuuhenkilo;
 
+-- Vastuuhenkilo also needs all basic CRUD operations on KATSOMO and KATSOMOPAIKKA
+GRANT SELECT, INSERT, UPDATE, DELETE ON KATSOMO TO vastuuhenkilo;
+GRANT SELECT, INSERT, UPDATE, DELETE ON KATSOMOPAIKKA TO vastuuhenkilo;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TAPAHTUMAKATSOMO TO vastuuhenkilo;
+
+-- For special ticket price offer management (if this is vastuuhenkilos job) vastuuhenkilo also needs
+-- basic CRUD on related tables
+GRANT SELECT, INSERT, UPDATE, DELETE ON HINTATARJOUS TO vastuuhenkilo;
+GRANT SELECT, INSERT, UPDATE, DELETE ON HINTATARJOUSLIPPU TO vastuuhenkilo;
+
+-- For ticket sale report generation RAPORTTI view is created
+-- More views can be created as needed (Like maybe a view for easy ticket printing containing both event name and place info)
 CREATE VIEW RAPORTTI AS
 SELECT 
 	TAPAHTUMA.*,
