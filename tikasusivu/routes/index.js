0926c96b18602ff37ebcce3ed00e9e617f075bd1
root <root@localhost.localdomain>
2016-01-31 16:54:08 +0000

    add real data to event view

--- tikasusivu/routes/index.js
+++ tikasusivu/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var Lippu = require('../models/lippu');
+var Tapahtuma = require('../models/tapahtuma');
+
 var session = require('express-session');
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
@@ -92,7 +95,16 @@ var tapahtumat = [{'nimi': 'tapahtuma1', 'paikka': 'areena'}, {'nimi': 'tapahtum
 
 /* GET event listing */
 router.get('/events', checkAuth, function(req, res, next){
-    res.render('event/index', {title: 'Tapahtumalistaus', events: tapahtumat});
+  Tapahtuma.fetchAll().then(function (events) {
+    if (events){
+      res.render('event/index', {title: 'Tapahtumalistaus', events: events.toJSON()});
+    } else {
+      res.status(404).json({error: 'EventNotFound'})
+    }
+  }).catch(function(err){
+    res.status(500).json({error: err});
+  });
+    
 });
 router.get('/api/tapahtumat', checkAuth);
 router.get('/api/v1/tapahtumat', checkAuth);
