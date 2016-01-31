81382772793373bf6cb25739ca2b053cc8eac3d5
root <root@localhost.localdomain>
2016-01-31 15:19:51 +0000

    add tapahtuma model

--- tikasusivu/routes/api.js
+++ tikasusivu/routes/api.js
@@ -2,10 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 var Lippu = require('../models/lippu');
+var Tapahtuma = require('../models/tapahtuma');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Lippu.fetchAll().then(function (collection) {
     console.log(collection.toJSON());
+  });
+  Tapahtuma.fetchAll().then(function (collection) {
+    console.log(collection.toJSON());
+    return collection.toJSON();
   });
 });
 
