495a30fd2eaa4d89e40b5402292bc818958642a9
Noora Vainio <nooraevainio@gmail.com>
2016-01-31 17:37:09 +0200

    tapahtumien listausta

--- tikasusivu/routes/index.js
+++ tikasusivu/routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'LippuLasse' });
 });
 
+var events = [{'nimi': 'tapahtuma1', 'paikka': 'hartwall areena'}, {'nimi': 'tapahtuma2', 'paikka': 'cupola'}];
+
+/* GET event listing */
+router.get('/events', function(req, res, next){
+    res.render('event/index', {title: 'Tapahtumalistaus', events: events});
+});
+
 module.exports = router;
