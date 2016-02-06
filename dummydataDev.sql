b05e44323943d4ed4e35c32c853a573913e3b794
Jaakko Husso <jaakko.husso@tut.fi>
2016-02-06 17:28:47 +0200

    Changes to dummydata and made some primary keys serial

--- dummydataDev.sql
+++ dummydataDev.sql
@@ -6,9 +6,9 @@ INSERT INTO POSTINUMERO VALUES('33200', 1);
 INSERT INTO POSTINUMERO VALUES('92100', 2);
 INSERT INTO POSTINUMERO VALUES('00200', 3);
 
-INSERT INTO OSOITE VALUES(1, '33200', 'Tieteenkatu 2 b 17');
-INSERT INTO OSOITE VALUES(2, '92100', 'Rantakatu 18');
-INSERT INTO OSOITE VALUES(3, '00200', 'Mannerheimintie 18');
+INSERT INTO OSOITE (Postinumero, Postiosoite) VALUES('33200', 'Tieteenkatu 2 b 17');
+INSERT INTO OSOITE (Postinumero, Postiosoite) VALUES('92100', 'Rantakatu 18');
+INSERT INTO OSOITE (Postinumero, Postiosoite) VALUES('00200', 'Mannerheimintie 18');
 
 INSERT INTO TAPAHTUMANJARJESTAJA VALUES(1, '8736412-1', 'tapahtumalaarnio', 2);
 INSERT INTO TAPAHTUMANJARJESTAJA VALUES(2, '4610760-4', 'tapahtumalasse', 3);
@@ -18,16 +18,20 @@ INSERT INTO SIHTEERI VALUES('sihteeri', 'salasana', 'Teemu Teekkari', '050123456
 INSERT INTO SIHTEERIJARJESTAJA VALUES('sihteeri', 1);
 INSERT INTO SIHTEERIJARJESTAJA VALUES('sihteeri', 2);
 
-INSERT INTO VASTUUHENKILO VALUES(1, 'laarniotar', 'laarnionsana', 'Vastuu Henkilo', '04011122244', 'vastuu@henkilo.fi', 2, 1, 'sihteeri');
-INSERT INTO VASTUUHENKILO VALUES(2, 'lasselaarnio', 'lassensana', 'Lasse Laarnio', '0501325355', 'lasse@laarnio.fi', 3, 2, 'sihteeri');
+INSERT INTO VASTUUHENKILO (Tunnus, Salasana, Nimi, Puhelin, Email, Osoite, Tapahtumanjarjestaja, Sihteeri)
+VALUES('laarniotar', 'laarnionsana', 'Vastuu Henkilo', '04011122244', 'vastuu@henkilo.fi', 2, 1, 'sihteeri');
+INSERT INTO VASTUUHENKILO (Tunnus, Salasana, Nimi, Puhelin, Email, Osoite, Tapahtumanjarjestaja, Sihteeri)
+VALUES('lasselaarnio', 'lassensana', 'Lasse Laarnio', '0501325355', 'lasse@laarnio.fi', 3, 2, 'sihteeri');
 
 INSERT INTO KATEGORIA VALUES(2, 'konsertti');
 INSERT INTO KATEGORIA VALUES(3, 'tanssi');
 INSERT INTO KATEGORIA VALUES(4, 'titesexboys');
 
 -- Tapahtuma
-INSERT INTO TAPAHTUMA VALUES(1, 'Laarnio konsertti', 3, 24,  '2016-02-15 10:23:06', '2016-02-15 13:23:06', 2, 1);
-INSERT INTO TAPAHTUMA VALUES(2, 'Tanssiesitys', 3, 8,  '2016-02-19 10:23:06', '2016-02-19 15:20:03', 3, 2);
+INSERT INTO TAPAHTUMA (Nimi, Kategoria, Alv, Alkuaika, Loppuaika, Osoiteid, Vastuuhenkilo)
+VALUES('Laarnio konsertti', 3, 24,  '2016-02-15 10:23:06', '2016-02-15 13:23:06', 2, 1);
+INSERT INTO TAPAHTUMA (Nimi, Kategoria, Alv, Alkuaika, Loppuaika, Osoiteid, Vastuuhenkilo)
+INSERT INTO TAPAHTUMA VALUES('Tanssiesitys', 3, 8,  '2016-02-19 10:23:06', '2016-02-19 15:20:03', 3, 2);
 
 -- Katsomot
 INSERT INTO KATSOMO VALUES(1, 'hartwallareena');
@@ -39,16 +43,24 @@ INSERT INTO TAPAHTUMAKATSOMO VALUES(2, 1);
 
 -- Hartwall areenan paikkoja
 -- Id, rivi, paikka, katsomoId
-INSERT INTO KATSOMOPAIKKA VALUES(1, 1, 1, 1);
-INSERT INTO KATSOMOPAIKKA VALUES(2, 1, 2, 1);
-INSERT INTO KATSOMOPAIKKA VALUES(3, 1, 3, 1);
-INSERT INTO KATSOMOPAIKKA VALUES(4, 1, 4, 1);
+INSERT INTO KATSOMOPAIKKA (Rivi, Paikka, Katsomo)
+VALUES(1, 1, 1);
+INSERT INTO KATSOMOPAIKKA (Rivi, Paikka, Katsomo)
+VALUES(1, 2, 1);
+INSERT INTO KATSOMOPAIKKA (Rivi, Paikka, Katsomo)
+VALUES(1, 3, 1);
+INSERT INTO KATSOMOPAIKKA (Rivi, Paikka, Katsomo)
+VALUES(4, 1, 4, 1);
 
 -- Seisomakatsomon paikkoja
-INSERT INTO KATSOMOPAIKKA VALUES(5, 6, 1, 2);
-INSERT INTO KATSOMOPAIKKA VALUES(6, 7, 2, 2);
-INSERT INTO KATSOMOPAIKKA VALUES(7, 8, 3, 2);
-INSERT INTO KATSOMOPAIKKA VALUES(8, 9, 4, 2);
+INSERT INTO KATSOMOPAIKKA (Rivi, Paikka, Katsomo)
+VALUES(6, 1, 2);
+INSERT INTO KATSOMOPAIKKA (Rivi, Paikka, Katsomo)
+VALUES(7, 2, 2);
+INSERT INTO KATSOMOPAIKKA (Rivi, Paikka, Katsomo)
+VALUES(8, 3, 2);
+INSERT INTO KATSOMOPAIKKA (Rivi, Paikka, Katsomo)
+VALUES(9, 4, 2);
 
 -- Lipputyypit
 INSERT INTO TYYPPI VALUES(1, 'opiskelija');
